2d81790c3baa02ed8fc348ce2b447b879be67544
Selen Erdoğan <selenerdogan2019@gtu.edu.tr>
2023-11-03 18:54:04 +0300

    main, manifest, xml, yaml first phase changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,168 @@
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:usb_serial/transaction.dart';
+import 'package:usb_serial/usb_serial.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAppState extends State<MyApp> {
+  UsbPort? _port;
+  String _status = "Idle";
+  List<Widget> _ports = [];
+  List<Widget> _serialData = [];
 
+  StreamSubscription<String>? _subscription;
+  Transaction<String>? _transaction;
+  UsbDevice? _device;
 
+  TextEditingController _textController = TextEditingController();
 
-  final String title;
+  Future<bool> _connectTo(device) async {
+    _serialData.clear();
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+    if (_subscription != null) {
+      _subscription!.cancel();
+      _subscription = null;
+    }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    if (_transaction != null) {
+      _transaction!.dispose();
+      _transaction = null;
+    }
 
-  void _incrementCounter() {
+    if (_port != null) {
+      _port!.close();
+      _port = null;
+    }
+
+    if (device == null) {
+      _device = null;
+      setState(() {
+        _status = "Disconnected";
+      });
+      return true;
+    }
+
+    _port = await device.create();
+    if (await (_port!.open()) != true) {
+      setState(() {
+        _status = "Failed to open port";
+      });
+      return false;
+    }
+    _device = device;
+
+    await _port!.setDTR(true);
+    await _port!.setRTS(true);
+    await _port!.setPortParameters(115200, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+    _transaction = Transaction.stringTerminated(_port!.inputStream as Stream<Uint8List>, Uint8List.fromList([13, 10]));
+
+    _subscription = _transaction!.stream.listen((String line) {
+      setState(() {
+        _serialData.add(Text(line));
+        if (_serialData.length > 20) {
+          _serialData.removeAt(0);
+        }
+      });
+    });
+
     setState(() {
+      _status = "Connected";
+    });
+    return true;
+  }
 
-      _counter++;
+  void _getPorts() async {
+    _ports = [];
+    List<UsbDevice> devices = await UsbSerial.listDevices();
+    if (!devices.contains(_device)) {
+      _connectTo(null);
+    }
+    print(devices);
+
+    devices.forEach((device) {
+      _ports.add(ListTile(
+          leading: Icon(Icons.usb),
+          title: Text(device.productName!),
+          subtitle: Text(device.manufacturerName!),
+          trailing: ElevatedButton(
+            child: Text(_device == device ? "Disconnect" : "Connect"),
+            onPressed: () {
+              _connectTo(_device == device ? null : device).then((res) {
+                _getPorts();
+              });
+            },
+          )));
+    });
+
+    setState(() {
+      print(_ports);
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    UsbSerial.usbEventStream!.listen((UsbEvent event) {
+      _getPorts();
+    });
+
+    _getPorts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _connectTo(null);
+  }
+
+  @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('USB Serial Plugin example app'),
+          ),
+          body: Center(
+              child: Column(children: <Widget>[
+                Text(_ports.length > 0 ? "Available Serial Ports" : "No serial devices available", style: Theme.of(context).textTheme.titleLarge),
+                ..._ports,
+                Text('Status: $_status\n'),
+                Text('info: ${_port.toString()}\n'),
+                ListTile(
+                  title: TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Text To Send',
+                    ),
+                  ),
+                  trailing: ElevatedButton(
+                    child: Text("Send"),
+                    onPressed: _port == null
+                        ? null
+                        : () async {
+                      if (_port == null) {
+                        return;
+                      }
+                      String data = _textController.text + "\r\n";
+                      await _port!.write(Uint8List.fromList(data.codeUnits));
+                      _textController.text = "";
+                    },
+                  ),
+                ),
+                Text("Result Data", style: Theme.of(context).textTheme.titleLarge),
+                ..._serialData,
+              ])),
+        ));
   }
 }
